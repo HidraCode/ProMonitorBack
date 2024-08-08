9bb11eaf7484d613801571db2f63a4ed3a3f0f14
Lucas Matias <lucasmatiassilva2022@gmail.com>
2024-08-08 09:49:46 -0300

    estrutura básica das pastas do front-end

--- ProMonitorFront/src/App.jsx
+++ ProMonitorFront/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react"
+import Login from "./pages/login/Login"
 
 function App() {
   return (
     <>
-      
+      <Login></Login>
     </>
   )
 }
---
+++ ProMonitorFront/src/pages/login/VerificarCodigo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const VerificarCodigo = () => {
+    return (
+        <></>
+    );
+};
+
+export default VerificarCodigo;
