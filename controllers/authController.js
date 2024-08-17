7fa0e665c98a4fe6c527a124b7634f9a2353d2fe
brenno-araujo25 <brennocaldeira@outlook.com>
2024-08-17 12:18:13 -0300

    feat: login de aluno e professor

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,10 +2,10 @@ import { loginUserService } from '../services/authService.js';
 
 export const authController = async (req, res) => {
     // Recupera email e senha no corpo da requisição
-    const { email, senha } = req.body;
+    const { email, senha, tipoUsuario } = req.body;
     try {
         // Chama o serviço de login
-        const user = await loginUserService(email, senha);
+        const user = await loginUserService(email, senha, tipoUsuario);
         res.json(user);
     } catch (error){
         res.status(401).json({ message: 'Credenciais inválidas!', error: error.message });
--- services/authService.js
+++ services/authService.js
@@ -6,34 +6,64 @@ const SALT_ROUNDS = 10;
 const JWT_SECRET = process.env.JWT_SECRET || 'secret_aqui';
 
 // Serviço para realizar login
-export const loginUserService = async (email, senha) => {
+export const loginUserService = async (email, senha, tipoUsuario) => {
     const connection = await pool.getConnection();
     try {
-        // Verifica se o email existe
-        const [user] = await connection.query('SELECT * FROM USUARIO WHERE email = ?', [email]);
-  
-        if (user.length === 0) {
-            throw new Error('Email ou senha incorretos');
+        if (tipoUsuario == 'aluno') {
+            console.log(email, senha, tipoUsuario);
+            const [aluno] = await connection.query('SELECT * FROM ALUNO JOIN USUARIO ON ALUNO.codigo_usuario = USUARIO.codigo_usuario WHERE USUARIO.email = ?', [email]);
+
+            if (aluno.length === 0) {
+                throw new Error('Email ou senha incorretos');
+            }
+
+            const alunoData = aluno[0];
+            console.log(alunoData)
+            // Compara a senha fornecida com a senha armazenada
+            const isPasswordValid = await bcrypt.compare(senha, alunoData.senha);
+        
+            if (!isPasswordValid) {
+                throw new Error('Email ou senha incorretos');
+            }
+        
+            // Cria o payload do token com o ID do usuário e outras informações 
+            const payload = { codigo_usuario: alunoData.codigo_usuario, email };
+            // Cria o token
+            const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+        
+            return {
+                codigo_usuario: alunoData.codigo_usuario,
+                email: alunoData.email,
+                token,
+            };
         }
-  
-        const userData = user[0];
-        // Compara a senha fornecida com a senha armazenada
-        const isPasswordValid = await bcrypt.compare(senha, userData.senha);
-    
-        if (!isPasswordValid) {
-            throw new Error('Email ou senha incorretos');
+        if (tipoUsuario == 'professor') {
+            const [professor] = await connection.query('SELECT * FROM PROFESSOR JOIN USUARIO ON PROFESSOR.codigo_usuario = USUARIO.codigo_usuario WHERE USUARIO.email = ?', [email]);
+
+            if (professor.length === 0) {
+                throw new Error('Email ou senha incorretos');
+            }
+
+            const professorData = professor[0];
+            console.log(alunoData)
+            // Compara a senha fornecida com a senha armazenada
+            const isPasswordValid = await bcrypt.compare(senha, professorData.senha);
+        
+            if (!isPasswordValid) {
+                throw new Error('Email ou senha incorretos');
+            }
+        
+            // Cria o payload do token com o ID do usuário e outras informações 
+            const payload = { codigo_usuario: professorData.codigo_usuario, email };
+            // Cria o token
+            const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
+        
+            return {
+                codigo_usuario: professorData.codigo_usuario,
+                email: professorData.email,
+                token,
+            };
         }
-    
-        // Cria o payload do token com o ID do usuário e outras informações 
-        const payload = { id: userData.codigo_usuario, email };
-        // Cria o token
-        const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
-    
-        return {
-            id: userData.codigo_usuario,
-            email: userData.email,
-            token,
-        };
     } catch (error) {
         throw new Error ('Erro ao realizar login: ' + error.message);
     } finally {
