b45891af687ef726a89c6689a133afa32ba73ee1
Vitória <jeanevitoria994@gmail.com>
2024-08-18 03:27:38 -0300

    Corrigindo as rotas

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,8 +6,8 @@ const router = express.Router();
 import { authController, verifyCodeController } from '../controllers/authController.js';
 
 // Define a rota de autenticação
-router.post('/auth/login', authController);
-router.post('/auth/pass-recovery/verify-code', verifyCodeController);
-router.post('/auth/pass-recovery', passwordRecoveryController)
+router.post('/login', authController);
+router.post('/pass-recovery/verify-code', verifyCodeController);
+router.post('/pass-recovery', passwordRecoveryController)
 
 export default router;
