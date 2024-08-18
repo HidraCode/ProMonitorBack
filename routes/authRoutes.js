5b1a1a2a27adc56993079c5bf5c9b859852a2f47
Vitória <jeanevitoria994@gmail.com>
2024-08-18 02:23:51 -0300

    Adicionando a lógica de redefinição de senha ao authService e authController.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 import { authController } from '../controllers/authController.js';
 
 // Define a rota de autenticação
-router.post('/login', authController);
+router.post('/auth/verify-code', authController);
+router.post('/auth/password-recovery/', authController)
 
 export default router;
--- services/authService.js
+++ services/authService.js
@@ -71,6 +71,106 @@ export const loginUserService = async (email, senha, tipoUsuario) => {
     }
 }
 
+//Cria um código para redefinição de senha
+export const createVerificationCode = async (userId) => {
+    const code = generateSixDigitCode();
+    const expiresAt = new Date(Date.now() + 3600000); // 1 hora de expiração
+
+    try {
+        const [result] = await pool.query(
+            'INSERT INTO verification_codes (user_id, code, expires_at) VALUES (?, ?, ?)',
+            [userId, code, expiresAt]
+        );
+        return { code, expiresAt };
+    } catch (error) {
+        console.error('Error creating verification code:', error);
+        throw new Error('Unable to create verification code');
+    }
+};
+
+export const sendMail = async (email) => {
+    const connection = await pool.getConnection();
+    try {
+        console.log(email);
+        const [user] = await connection.query('SELECT * FROM USUARIO WHERE USUARIO.email = ?', [email]);
+
+        if (user.length === 0) {
+            throw new Error('Nenhum usuário cadastrado com o e-mail informado.');
+        }
+
+        const userData = user[0];
+        console.log(userData)
+
+        const [codeData] = await createVerificationCode(userData.id)
+
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 587,
+
+            auth: {
+                user: "promonitorufrpe@gmail.com",
+                pass: "oiinyikrxaihamqg",
+            },
+
+        });
+
+        const emailBody = <p>Recuperação de Senha</p>
+
+        const mailOptions = {
+            from: "promonitorufrpe@gmail.com",
+            to: email,
+            subject: "Código de recuperação de senha ProMonitor",
+            text: "Olá, seu código de verificação do ProMonitor: " + codeData.code + ". Esse código expira em 1 hora.",
+            html: emailBody,
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.error("Erro ao enviar o e-mail:", error);
+            } else {
+                console.log("E-mail enviado:", info.res);
+            }
+        });
+
+    }
+    catch (error) {
+        throw new Error('Erro ao enviar código de verificação: ' + error.message);
+    }
+    finally {
+        connection.release();
+    }
+}
+
+export const verifyCode = async (email, code) => {
+    const connection = await pool.getConnection();
+    try {
+        const [user] = await connection.query('SELECT * FROM USUARIO WHERE USUARIO.email = ?', [email])
+        if (user.length === 0) {
+            console.log("Usuário não encontrado.")
+        }
+        const userData = user[0];
+        console.log(userData)
+
+        const [results] = await pool.query(
+            'SELECT * FROM verification_codes WHERE user_id = ? AND code = ? AND expires_at > NOW()',
+            [userData.id, code]
+        );
+
+        if (results.length === 0) {
+            throw new Error('Código inválido ou expirado.');
+        }
+
+        // Código é válido
+        return true;
+    }
+    catch (error) {
+        throw new Error('Erro ao validar o código de verificação: ' + error.message);
+    } finally {
+        connection.release();
+    }
+}
+
 // Função para verificar token
 export const verifyToken = async (token) => {
     try {
