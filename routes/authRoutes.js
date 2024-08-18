56a145aa0012fbaa5dd5c13c0e0c10c1ad6905eb
Vitória <jeanevitoria994@gmail.com>
2024-08-18 02:59:56 -0300

    Corrigindo rotas

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,7 +7,7 @@ import { authController, verifyCodeController } from '../controllers/authControl
 
 // Define a rota de autenticação
 router.post('/auth/login', authController);
-router.post('/auth/verify-code', verifyCodeController);
+router.post('/auth/pass-recovery/verify-code', verifyCodeController);
 router.post('/auth/pass-recovery', passwordRecoveryController)
 
 export default router;
